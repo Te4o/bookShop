e4b4c316e5d17f0be1940c24648a789c8e6c9cd1
Стоян Димитров <sdimitrov@ad.mapex.bg>
2018-01-12 23:44:31 +0200

    adding project

---
+++ reduxApp/src/app.js
@@ -0,0 +1,48 @@
+"use strict"
+// REACT
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+
+// IMPORT COMBINED REDUCERS
+import reducers from './reducers/index';
+// IMPORT ACTIONS
+import { addToCart } from './actions/cartActions';
+import { postBooks, deleteBooks, updateBooks } from './actions/booksActions';
+
+// STEP 1 create the store
+const middleware = applyMiddleware(logger);
+const store = createStore(reducers, middleware);
+
+import BooksList from './components/pages/booksList';
+
+render(
+    <Provider store={store}>
+        <BooksList />
+    </Provider>, document.getElementById('app')
+);
+
+// STEP 2 create and dispatch actions
+// store.dispatch(postBooks(
+    
+// ))
+
+// // DELETE а боок
+// store.dispatch(deleteBooks(
+//     { id: 1 }
+// ))
+
+// // UPDATE а боок
+// store.dispatch(updateBooks(
+//     {
+//         id: 2,
+//         title: 'Learn React in 24h'
+//     }
+// ))
+
+// //-->> CART ACTIONS <<--
+// // ADD to cart
+// store.dispatch(addToCart([{ id: 1 }]))
